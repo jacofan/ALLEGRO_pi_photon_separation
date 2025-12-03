8aea1069ce78da33564c42f976da70b1a22e5d82
Jacopo Fanini <jacopo.fanini@cern.ch>
2025-12-03 21:20:35 +0100

    Remove (hopefully) unneccessary

--- photon_pi0_allegro/src/utils/load_pretrained_models.py
+++ photon_pi0_allegro/src/utils/load_pretrained_models.py
@@ -5,13 +5,13 @@ def load_train_model(args, dev):
     if args.load_model_weights is not None and args.correction:
         model = load_trainer(args, dev)
     elif args.load_model_weights is not None:
-        from src.models.GATr.Gatr_pf_e_noise import ExampleWrapper as GravnetModel
+        from src.models.GATr.Gatr_pf_e_binary import ExampleWrapper as GravnetModel
         model = GravnetModel.load_from_checkpoint(
             args.load_model_weights, args=args, dev=0, map_location=dev,strict=False)
     return model 
 
 def load_trainer(args, dev):
-    from src.models.GATr.Gatr_pf_e_noise import ExampleWrapper as GravnetModel
+    from src.models.GATr.Gatr_pf_e_binary import ExampleWrapper as GravnetModel
     model = GravnetModel.load_from_checkpoint(
         args.load_model_weights, args=args, dev=0, map_location=dev,strict=False)
     print("weight before checkpoint", model.ec_model_wrapper_neutral.gatr.linear_out.s2mvs.weight)
@@ -32,14 +32,14 @@ def load_trainer(args, dev):
 
 def load_test_model(args, dev):
     if args.load_model_weights is not None and args.correction:
-            from src.models.GATr.Gatr_pf_e_noise import ExampleWrapper as GravnetModel
+            from src.models.GATr.Gatr_pf_e_binary import ExampleWrapper as GravnetModel
             model = GravnetModel.load_from_checkpoint(
                 args.load_model_weights, args=args, dev=0, map_location=dev, strict=False
             )
-            model2 = GravnetModel.load_from_checkpoint(args.load_model_weights_clustering, args=args, dev=0, strict=False, map_location=torch.device("cuda:3")) # Load the good clustering
-            model.gatr = model2.gatr
-            model.ScaledGooeyBatchNorm2_1 = model2.ScaledGooeyBatchNorm2_1
-            model.clustering = model2.clustering
-            model.beta = model2.beta
+            ## model2 = GravnetModel.load_from_checkpoint(args.load_model_weights_clustering, args=args, dev=0, strict=False, map_location=torch.device("cuda:3")) # Load the good clustering
+            ## model.gatr = model2.gatr
+            ## model.ScaledGooeyBatchNorm2_1 = model2.ScaledGooeyBatchNorm2_1
+            ## model.clustering = model2.clustering
+            ## model.beta = model2.beta
     return model 
 
